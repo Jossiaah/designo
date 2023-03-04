53d5e53596e4ee8a847a2d9726d346e8ad7d5e7d
Josiah <jcast1998@gmail.com>
2023-03-03 18:27:19 -0800

    Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
+import logoImg from '../assets/shared/desktop/logo-dark.png';
+import hamIcon from '../assets/shared/mobile/icon-hamburger.svg'
+
+const Container = styled.div`
+    z-index: 2;
+
+`;
+const Logo = styled.img`
+    width: 200px;
+`;
+const Nav = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 20px;
+    padding-top: 40px;
+    padding-left: 30px;
+    
+`;
+
+const MenuButton = styled.img`
+width: 30px;
+
+`;
+
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <Container>
+            <Nav>
+                <Logo src={logoImg} />
+                <MenuButton src={hamIcon} />
+            </Nav>
+        </Container>
     )
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Hero from '../Components/Hero'
 import Navbar from '../Components/Navbar'
 
 const Home = () => {
     return (
         <>
             <Navbar />
+            <Hero />
 
 
 
